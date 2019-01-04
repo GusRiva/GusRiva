f971e425cd909ea41730e4dd877de23a713026eb
GusRiva <gusfer@gmail.com>
2019-01-04 20:26:26 +0000

    cv

--- js/cv.js
+++ js/cv.js
@@ -3,16 +3,17 @@
     var section_status = [1,1];
     
     $("div.section").hide();
+    $("div.hide").hide();
     
     $("[class*='toggler']").click(function () {
         var index = $(this).attr("class").substr(8)
         if (section_status[index] == 0){
            $(this).parent().find("div[n=" + index +"]").hide();
-           $(this).find("a[n=" + index +"]").hide();
+           $(this).find("a[class='show'][n=" + index +"]").hide();
            section_status[index] = 1;
         }else{
             $(this).parent().find("div[n=" + index +"]").show();
-            $(this).find("a[n=" + index +"]").show();
+            $(this).find("a[class='show'][n=" + index +"]").show();
             section_status[index] = 0;
         };
     });
