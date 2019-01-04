6f25832da41c3c0c94355c046fd3b047ce5ae43f
GusRiva <gusfer@gmail.com>
2019-01-04 19:46:06 +0000

    cv

--- js/cv.js
+++ js/cv.js
@@ -1,8 +1,9 @@
 ﻿$(document).ready(function () {
     //section_status has the info of the status of the divs with the different sections. 0 means visible, 1 means hidden.
-    section_status = [0,0]
+    var section_status = [0,0]
     $('.toggler').click(function () {
-        console.log($(this).index());
+        var index = $(this).attr("class").substr(8)
+        console.log(index);
         $(this).parent().find('div').hide();
     });
     
