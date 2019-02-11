f994beed60469166c67cba1ee58319e098a74f09
GusRiva <gusfer@gmail.com>
2019-02-11 18:18:46 +0000

    wld first draft

---
+++ wld/js/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    previousSelectedTopic = ""
+/*        Hide other tables*/
+    $(".other-text-table *").hide();
+/*    When click on topic selector*/
+    $(".topic-selector").click( function(){
+        topicNumber = $(this).attr("id").substr(1);
+        if (topicNumber == previousSelectedTopic){
+            $(this).css("color","black");
+            $("span[class='text-markup'][title='"+topicNumber+"']").css("color","black");
+            $(".other-text-table *").hide();
+            previousSelectedTopic = "";
+        }
+        else {
+            $(".topic-selector").css("color","black");
+            $(this).css("color","blue");
+            $("span[class='text-markup'][title*='"+previousSelectedTopic+"']").css("color","black");
+            $("span[class='text-markup'][title*='"+topicNumber+"']").css("color","blue").show();
+            $(".other-text-table *").show();
+            $(".other-text-table tr").not("tr[class*='"+topicNumber+"']").hide();
+            
+           
+            previousSelectedTopic = topicNumber;
+        };
+        
+    });
+
+
+});
