f5ed5b562965d12c90043717bdb48078f05f8c0d
GusRiva <gusfer@gmail.com>
2019-02-24 16:45:12 +0000

    Love Debates Draft updated

--- wld/js/main.js
+++ wld/js/main.js
@@ -3,6 +3,7 @@
 /*        Hide other tables*/
     $(".other-text-table *").hide();
 /*    When click on topic selector*/
+
     $(".topic-selector").click( function(){
         topicNumber = $(this).attr("id").substr(1);
         if (topicNumber == previousSelectedTopic){
@@ -19,11 +20,22 @@
             $(".other-text-table *").show();
             $(".other-text-table tr").not("tr[class*='"+topicNumber+"']").hide();
             
-           
             previousSelectedTopic = topicNumber;
         };
-        
+    });
+    
+/*    When cicking on expand*/
+    $(".expand").click(function(){
+        alert("When clicking here more verses of these witness before and after the selected topic would be displayed in the column.");
+    });
+    
+/*    When clicking on the super-container selector buttons*/
+    $("button[id*='b_sc']").click(function(){
+        option_selected = $(this).attr("id").split("_")[2];
+        $("div[class='super-container']").hide();
+        $("div[id='sc_"+option_selected+"']").show();
     });
 
-
+    
+    
 });
